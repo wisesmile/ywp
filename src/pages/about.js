f6f83bc46209663deca864bbdc57e906cb717f9e
Matt Pazaryna <mpazaryna@gmail.com>
2020-04-04 18:54:41 -0400

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="About" />
+    <p>
+      I was introduced to the practice of meditation in 1982. This practice was
+      the gateway to a lifelong study of Vedanta Philosophy and asana practice.
+      I taught my first class in 1985.
+    </p>
+    <p>
+      In 1986 I moved to Washington DC and enrolled in classes at various yoga
+      centers throughout town. I began attending weekly services at The Self
+      Revelation Church of Absolute Monism in Bethesda Maryland and accelerated
+      my studies. Weekly services highlight Advaita Vedanta, the practice of
+      Kriya Yoga and the Universality of Soul. The teachings, lessons,
+      instruction, and counsel remain steadfast and have an active presence in
+      my life.
+    </p>
+    <p>
+      In 2015, I returned to New Jersey and deepened my commitment to the
+      practices and study. I’ve met many beautiful souls and teachers that
+      inspire my practice. In 2017 in completed my Yoga Teacher Training
+      200-hour certification and immediately began teaching.
+    </p>
+    <p>
+      My interests are varied; I enjoy teaching beginning students, individuals
+      with special needs and those striving for the sublime spiritual connection
+      of asana, breath, and mediation. I remain inspired by the writings of
+      Swami Premananda, Srimati Kamala (Swami Kamalandanda), Swami
+      Satchidananda, BKS Iyengar, and Pema Chodron. I’m the proud father of two
+      amazing children and wake up grateful. I work as a software engineer and
+      have a love of the arts, playing and enjoying music.
+    </p>
+    <Link to="/">Home</Link>
+  </Layout>
+)
+
+export default About
