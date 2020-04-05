a8e12f45e09ccb7b79420728eee1379faef21f72
Matt Pazaryna <mpazaryna@gmail.com>
2020-04-05 07:54:51 -0400

    more updates to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,19 +8,23 @@ const About = () => (
     <SEO title="About" />
     <p>
       In 1982, I began a formal practice of meditation. This opened the door
-      onto the study of Vedanta Philosophy and Hatha Yoga and a curiosity about
-      wellness. I taught my first Hatha Yoga class in 1985.
+      into a study of Vedanta Philosophy, Yoga and wellness. I taught my first
+      Hatha Yoga class in 1985.
     </p>
     <p>
       In 1986 I moved to Washington DC and attended classes at yoga centers
-      throughout town. I attended services at The Self Revelation Church of
-      Absolute Monism and immersed myself deeper into study and practice. The
-      weekly services and programs focused on Advaita Vedanta, Kriya Yoga and
-      teachings regarding Universality of Soul.
+      throughout town. I participated at{" "}
+      <a href="http://www.selfrevelationchurch.org/" target="_new">
+        The Self Revelation Church of Absolute Monism
+      </a>{" "}
+      and immersed myself deeper into study and practice. The weekly services
+      and programs focused on Advaita Vedanta, Kriya Yoga and Universality of
+      Soul. I volunteered, worked on the grounds and was a member of the Bell
+      Choir.
     </p>
     <p>
-      I returned to Jersey in 2015 and discovered many studios close to home. In
-      2017 in completed my Yoga Teacher Training 200-hour certification. I
+      I returned to Jersey in 2015 and discovered many studios close to home. I
+      completed my Yoga Teacher Training 200-hour certification in 2017. I
       immediately began teaching in several local studios.
     </p>
     <p>
