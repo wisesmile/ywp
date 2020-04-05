f33db911311258fe7fe5d755769536741c661cc0
Matt Pazaryna <mpazaryna@gmail.com>
2020-04-05 07:44:45 -0400

    More updates for the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,30 +7,28 @@ const About = () => (
   <Layout>
     <SEO title="About" />
     <p>
-      I was introduced to the practice of meditation in 1982. This practice was
-      the gateway to a lifelong study of Vedanta Philosophy and asana practice.
-      I taught my first class in 1985.
+      In 1982, I began a formal practice of meditation. This opened the door
+      onto the study of Vedanta Philosophy and Hatha Yoga and a curiosity about
+      wellness. I taught my first Hatha Yoga class in 1985.
     </p>
     <p>
-      In 1986 I moved to Washington DC and enrolled in classes at various yoga
-      centers throughout town. I began attending weekly services at The Self
-      Revelation Church of Absolute Monism in Bethesda Maryland and accelerated
-      my studies. Weekly services highlight Advaita Vedanta, the practice of
-      Kriya Yoga and the Universality of Soul. The teachings, lessons,
-      instruction, and counsel remain steadfast and have an active presence in
-      my life.
+      In 1986 I moved to Washington DC and attended classes at yoga centers
+      throughout town. I attended services at The Self Revelation Church of
+      Absolute Monism and immersed myself deeper into study and practice. The
+      weekly services and programs focused on Advaita Vedanta, Kriya Yoga and
+      teachings regarding Universality of Soul.
     </p>
     <p>
       I returned to Jersey in 2015 and discovered many studios close to home. In
-      2017 in completed my Yoga Teacher Training 200-hour certification and
-      immediately began teaching.
+      2017 in completed my Yoga Teacher Training 200-hour certification. I
+      immediately began teaching in several local studios.
     </p>
     <p>
       My teaching and practice interests are varied; I have a focus on beginning
       students and those with special needs. I am inspired by the writings of
-      Swami Kamalandanda (Srimati Kamala), Swami Satchidananda, BKS Iyengar, and
-      Pema Chodron. I’m the proud father of two amazing children.
+      Swami Kamalandanda, Swami Satchidananda, BKS Iyengar, and Pema Chodron.
     </p>
+    <p>I’m the proud father of two amazing children.</p>
     <Link to="/">Home</Link>
   </Layout>
 )
